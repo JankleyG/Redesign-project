5bec74ecde557c8e84e85f1d79e01b1cb861cadd
dennisllng <dennisllng@yahoo.com>
2017-11-02 20:08:20 -0700

    angel page

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 $(document).foundation()
+$('.clickOn').delegate("clickOn","click", function() {
+var id = $(this).attr('thisid')
+     if ( thisid == "clickImg" ) {
+-webkit-filter:grayscale(0%);
+       filter: grayscale(0%);
+     }
+ });
